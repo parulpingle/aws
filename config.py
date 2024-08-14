5fd753817d0a1c748ed9c5f881849246f326d6c2
parulpingle <114743095+parulpingle@users.noreply.github.com>
2024-08-15 00:42:50 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employeenew.crie2ki4ey74.ap-south-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "Sujit1069"
+customdb = "employee"
+custombucket = "plusemployee"
+customregion = "ap-south-1"
